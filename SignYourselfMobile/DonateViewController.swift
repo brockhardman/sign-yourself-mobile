1987d5cfb47e8ceabd00b964301a1a398353e412
Brock Hardman <brockhardman@Brocks-MBP-2.home>
2018-09-29 15:04:36 -0500

    comment unused code

--- SignYourselfMobile/DonateViewController.swift
+++ SignYourselfMobile/DonateViewController.swift
@@ -27,48 +27,48 @@ class DonateViewController: UIViewController {
     
 }
 
-class IntentHandler: INExtension, INRequestPaymentIntentHandling {
-    
-    let paymentHandler = PaymentHandler()
-    
-    func handle(intent: INRequestPaymentIntent, completion: @escaping (INRequestPaymentIntentResponse) -> Void) {
-        
-        var response = INRequestRideIntentResponse(code: .success, userActivity: nil)
-        let rideStatus = INRideStatus()
-        
-        // Apple Pay Payment
-        
-        paymentHandler.startPayment() { (success) in
-            
-            if success {
-                
-                var driverName = PersonNameComponents()
-                
-                driverName.givenName = "Johnny"
-                
-                driverName.familyName = "A"
-                
-                let driverHandle = INPersonHandle(value: "john-appleseed@mac.com", type: .emailAddress)
-                
-                
-                
-                rideStatus.driver = INRideDriver(personHandle: driverHandle, nameComponents: driverName, displayName: nil, image: nil, contactIdentifier: nil, customIdentifier: nil)
-                
-            } else {
-                
-                response = INRequestRideIntentResponse(code: .failure, userActivity: nil)
-                
-            }
-            
-            completion(response)
-            
-        }
-    }
-    
-    
-    func resolveUsesApplePayForPayment(forRequestRide intent: INRequestRideIntent, with completion: (INBooleanResolutionResult) -> Void) {
-        
-        completion(INBooleanResolutionResult.success(with: true))
-        
-    }
-}
+//class IntentHandler: INExtension, INRequestPaymentIntentHandling {
+//
+//    let paymentHandler = PaymentHandler()
+//
+//    func handle(intent: INRequestPaymentIntent, completion: @escaping (INRequestPaymentIntentResponse) -> Void) {
+//
+//        var response = INRequestRideIntentResponse(code: .success, userActivity: nil)
+//        let rideStatus = INRideStatus()
+//
+//        // Apple Pay Payment
+//
+//        paymentHandler.startPayment() { (success) in
+//
+//            if success {
+//
+//                var driverName = PersonNameComponents()
+//
+//                driverName.givenName = "Johnny"
+//
+//                driverName.familyName = "A"
+//
+//                let driverHandle = INPersonHandle(value: "john-appleseed@mac.com", type: .emailAddress)
+//
+//
+//
+//                rideStatus.driver = INRideDriver(personHandle: driverHandle, nameComponents: driverName, displayName: nil, image: nil, contactIdentifier: nil, customIdentifier: nil)
+//
+//            } else {
+//
+//                response = INRequestRideIntentResponse(code: .failure, userActivity: nil)
+//
+//            }
+//
+//            completion(response)
+//
+//        }
+//    }
+//
+//
+//    func resolveUsesApplePayForPayment(forRequestRide intent: INRequestRideIntent, with completion: (INBooleanResolutionResult) -> Void) {
+//
+//        completion(INBooleanResolutionResult.success(with: true))
+//
+//    }
+//}
